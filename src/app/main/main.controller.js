87195bc20aa9da285a3541970ccb9dc94a91644d
kongxiangwen <xw.kong@hnair.com>
2017-04-06 14:36:24 +0800

    add ngAnimate and ngRoute support

---
+++ src/app/main/main.controller.js
@@ -0,0 +1,58 @@
+/**
+ * app main controller
+ */
+
+(function () {
+    'use strict';
+
+    angular.module('sparrow').controller('MainController', MainController);
+
+    /** @ngInject */
+    function MainController($rootScope, $scope, $timeout) {
+       /* jshint validthis: true */
+
+       $scope.name = 'sparrow';
+  
+       var theme_11 = {
+          themeID: 11,
+          navbarHeaderColor: 'bg-info',
+          navbarCollapseColor: 'bg-white',
+          asideColor: 'bg-dark b-r',
+          headerFixed: true,
+          asideFixed: false,
+          asideFolded: false,
+          asideDock: false,
+          container: false
+      };
+
+      $scope.app = {
+        name: 'sparrow',
+        version: '',
+        // for chart colors
+        color: {
+          primary: '#7266ba',
+          info:    '#23b7e5',
+          success: '#27c24c',
+          warning: '#fad733',
+          danger:  '#f05050',
+          light:   '#e8eff0',
+          dark:    '#3a3f51',
+          black:   '#1c2b36'
+        },
+        settings: theme_11
+      };
+
+        function showToastr() {
+            toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
+            vm.classAnimation = '';
+        }
+
+       
+
+     
+        // vm.clearItems = function() {
+        //   $rootScope.pageRoutes = [];
+        // }
+    }
+})();
+
---
+++ src/js/app.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+    // Declare app level module which depends on views, and components
+    angular
+        .module('sparrow', [
+        /* Angular Modules */
+        'ngAnimate',
+        //'ngMessages',
+        //'ngAria',
+        'ngRoute',
+        /* 3rd-Party Modules 
+        'ui.bootstrap',
+        'ui.router',
+        'ui.bootstrap.tpls',
+        'ui.jq',
+        'ui.load',
+
+        'toastr',
+        'pascalprecht.translate',
+        'restangular',
+        'smart-table',
+        'iot.ui.directives',
+        'xeditable',
+        'easypiechart'*/
+    ]);
+
+})();
